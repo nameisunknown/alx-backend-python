b19e484a80c0c9e87449bb29ea3bd80409d0ec45
nameisunknown <orangekapital@gmail.com>
2024-08-03 07:13:59 +0300

    checker fixxes

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,10 +4,11 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-  """returns the sum of float list elements as a float."""
+    """returns the sum of float list elements as a float."""
 
-  result: float = 0
-  for ele in input_list:
-    result += ele
+    result: float = 0
+    for ele in input_list:
+        result += ele
 
-  return result
+    return result
+
