5d816a19443f338634c28763a98869e5ba7851e5
nameisunknown <orangekapital@gmail.com>
2024-08-03 06:29:38 +0300

    Basic annottaions to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains to_str() function"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of the float param n"""
+    return str(n)
